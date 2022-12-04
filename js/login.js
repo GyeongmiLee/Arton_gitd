e7f4f40fbac4211ef8812c2d0cea8a3d5c29997d
Your Name <qhsxnqlt@naver.com>
2022-12-04 17:58:50 +0900

    Update login.js

--- js/login.js
+++ js/login.js
@@ -1,29 +1,20 @@
 $(document).ready(function(){
-// alert("d");
 
-// $(document).on('click','#ttt',function(){
-//     console.log($('#id').val());
-//     console.log($('#pw').val());
-// })
 });
 let login_msg = $('.login_msg');
 function form_chk(frm){
-    // console.log(frm.$('id'));
-    // console.log(frm.$('id').val());
-
-    if(frm.id.value.length == 0 && frm.pw.value.length == 0){
-        set_message('아이디와 패스워드는 필수항목입니다',frm.id);
-        // alert("d1");
+    const reg_id = /^[a-z0-9_-]{5,20}$/;
+    const reg_pw = /(?=.*[a-zA-ZS])(?=.*?[#?!@$%^&*-]).{8,16}/; 
+    if(!reg_pw.test(frm.pw.value) && !reg_id.test(frm.id.value)){
+        set_message('아이디 또는 패스워드가 맞지 않는 형식입니다.',frm.id);
         return false;
     }
     else if(frm.id.value.length == 0){
         set_message('id를 입력해주세요',frm.id);
-        // alert("d2");
         return false;
     }
     else if(frm.pw.value.length == 0){
         set_message('pw를 입력해주세요',frm.pw);
-        // alert("d3");
         return false;
     }
     return true;
@@ -31,7 +22,6 @@ function form_chk(frm){
 //trim 적용하면 좋을듯 (세현의견)
 function set_message(msg,e){
     login_msg.text(msg);
-    // alert("d4");
     if(e){
          e.select();
     }
